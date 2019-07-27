11df5e4f08086723a8dcc7cb58187fa97619a920
QuantumT95 <tran.charlie@ymail.com>
2019-07-27 20:56:38 +0700

    updated pics and made it into gifs

--- js/index.js
+++ js/index.js
@@ -1,46 +1,5 @@
 new WOW().init();
 
-$(function () {
-    $('#sytImg').click(function (e) {
-        if ($(this).attr("src") === "./images/SYT1.png")
-            $(this).attr("src", "./images/SYT2.png");
-        else if ($(this).attr("src") === "./images/SYT2.png")
-            $(this).attr("src", "./images/SYT1.png");
-    });
-});
-
-$(function () {
-    $('#bfImg').click(function (e) {
-        if ($(this).attr("src") === "./images/BFLifeCounter1.png")
-            $(this).attr("src", "./images/BFLifeCounter2.png");
-        else if ($(this).attr("src") === "./images/BFLifeCounter2.png")
-            $(this).attr("src", "./images/BFLifeCounter.png");
-        else if ($(this).attr("src") === "./images/BFLifeCounter.png")
-            $(this).attr("src", "./images/BFLifeCounter1.png");
-    });
-});
-
-
-$(function () {
-    $('#vgImg').click(function (e) {
-        if ($(this).attr("src") === "./images/vgBoard1.png")
-            $(this).attr("src", "./images/vgBoard.png");
-        else if ($(this).attr("src") === "./images/vgBoard.png")
-            $(this).attr("src", "./images/vgBoard1.png");
-    });
-});
-
-$(function () {
-    $('#imoImg').click(function (e) {
-        if ($(this).attr("src") === "./images/immowow.png")
-            $(this).attr("src", "./images/immowow1.png");
-        else if ($(this).attr("src") === "./images/immowow1.png")
-            $(this).attr("src", "./images/immowow2.png");
-        else if ($(this).attr("src") === "./images/immowow2.png")
-            $(this).attr("src", "./images/immowow.png");
-    });
-});
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
