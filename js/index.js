4648d9f04fa68600b8a0fbc973fb3f5fd8669967
QuantumT95 <tran.charlie@ymail.com>
2019-08-11 19:52:58 -0500

    navbar, trying to find a better way to sticky to top and highlight current section

--- js/index.js
+++ js/index.js
@@ -16,3 +16,31 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(function () {
+    var url = window.location.href;
+    var activePage = url;
+    $('#nav_bar a').each(function () {
+        var linkPage = this.href;
+
+        if (activePage == linkPage) {
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 280) {
+            $('#nav_bar').addClass('navbar-fixed');
+        }
+        if ($(window).scrollTop() < 281) {
+            $('#nav_bar').removeClass('navbar-fixed');
+        }
+    });
+});
